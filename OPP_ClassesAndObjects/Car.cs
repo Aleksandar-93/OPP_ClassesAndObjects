ae4e99c909421b41775993b67d92a6c8b5551af4
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-19 23:09:55 +0200

    Car () class

---
+++ OPP_ClassesAndObjects/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP_ClassesAndObjects
+{
+    internal class Car
+    {
+        // constructor
+        public Car() {
+            Console.WriteLine("Car was created");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("car stoped!");
+        }
+    }
+}
--- OPP_ClassesAndObjects/Program.cs
+++ OPP_ClassesAndObjects/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OPP_ClassesAndObjects;
+
+namespace ClassesAndObjects
+{
+
+    //A class in C# (and most other object-oriented programming languages)
+    //is a blueprint for creating objects.
+    //It defines a set of properties (data attributes)
+    //and methods (functions) that an object of that class will have.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car();
+            audi.Drive();
+            Car bmw = new Car();
+            Console.WriteLine("press 1 to stop car!");
+            string userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                audi.Stop();
+            }
+            else
+            {
+                Console.WriteLine("car drives indefinetly");
+            }
+        }
+
+    }
+}
